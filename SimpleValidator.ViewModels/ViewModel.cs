a7256fb7daff89d98eec6c0ce38a166e39b08cca
vadimffe <vadim.ffegmail.com>
2020-12-21 20:56:42 +0200

    modifications

--- SimpleValidator.ViewModels/ViewModel.cs
+++ SimpleValidator.ViewModels/ViewModel.cs
@@ -9,16 +9,23 @@ namespace SimpleValidator.ViewModels
 {
     public class ViewModel : ObservableObject, IDataErrorInfo
     {
+        private string MyDefaultTime = "22:32";
+        private string MyDefaultInterval = "3";
         public ICommand StopManualTimerCommand => new RelayCommand(param => this.SaveSettings());
 
         private void SaveSettings()
         {
-            if (ValidateTime(Username))
+            if (ValidateTime(TimeLimit))
                 MessageBox.Show("Save");
         }
 
+        public ViewModel()
+        {
+            this.TimeLimit = this.MyDefaultTime;
+            this.TimeRecordInterval = this.MyDefaultInterval;
+        }
+
         public string Error { get { return null; } }
-        private string _username;
 
         public Dictionary<string, string> ErrorCollection { get; private set; } = new Dictionary<string, string>();
 
@@ -30,6 +37,11 @@ namespace SimpleValidator.ViewModels
                                           DateTimeStyles.None,
                                           out ignored);
         }
+        public bool ValidateNumber(string time)
+        {
+            int ignored;
+            return int.TryParse(time, out ignored);
+        }
 
         public string this[string name]
         {
@@ -39,30 +51,53 @@ namespace SimpleValidator.ViewModels
 
                 switch (name)
                 {
-                    case "Username":
-                        if (string.IsNullOrWhiteSpace(Username))
-                            result = "Username cannot be empty";
-                        else if (!ValidateTime(Username))
-                            result = "Username must be a minimum of 5 characters.";
+                    case "TimeLimit":
+                        if (string.IsNullOrWhiteSpace(TimeLimit))
+                            result = "This field cannot be empty";
+                        else if (!ValidateTime(TimeLimit))
+                            result = "This field can contain only time format HH:mm";
+                        break;
+
+                    case "TimeRecordInterval":
+                        if (string.IsNullOrWhiteSpace(this.TimeRecordInterval))
+                        {
+                            result = "This field cannot be empty";
+                        }
+                        else if (!this.ValidateNumber(this.TimeRecordInterval))
+                        {
+                            result = "Field can contain only numbers";
+                        }
+
                         break;
                 }
 
-                if (ErrorCollection.ContainsKey(name))
-                    ErrorCollection[name] = result;
+                if (this.ErrorCollection.ContainsKey(name))
+                    this.ErrorCollection[name] = result;
                 else if (result != null)
                     ErrorCollection.Add(name, result);
 
-                OnPropertyChanged(nameof(ErrorCollection));
+                this.OnPropertyChanged(nameof(this.ErrorCollection));
                 return result;
             }
         }
 
-        public string Username
+        private string _timeRecordInterval;
+        public string TimeRecordInterval
         {
-            get { return _username; }
+            get { return this._timeRecordInterval; }
             set
             {
-                OnPropertyChanged(ref _username, value);
+                OnPropertyChanged(ref this._timeRecordInterval, value);
+            }
+        }
+
+        private string _timeLimit;
+        public string TimeLimit
+        {
+            get { return this._timeLimit; }
+            set
+            {
+                OnPropertyChanged(ref this._timeLimit, value);
             }
         }
 
